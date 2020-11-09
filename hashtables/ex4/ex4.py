133ef0bf2eb7f1b48d04146037f9574a870a2778
Craig Swanson <cswanson02@me.com>
2020-11-08 20:17:49 -0600

    Completed ex4 challenge.

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,15 +1,24 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+
+    positive_numbers = {}
+    negative_numbers = {}
+    result = []
+
+    for element in a:
+        if element >= 0:
+            if element not in positive_numbers:
+                positive_numbers[element] = True
+        elif element < 0:
+            element = element * -1
+            if element not in negative_numbers:
+                negative_numbers[element] = True
+    
+    for key in positive_numbers:
+        if key in negative_numbers:
+            result.append(key)
 
     return result
 
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
-
-
-# Example input:
-# [ 1, -1, 2, 3, -4, -3, 4, -5, 6, 7 ]
