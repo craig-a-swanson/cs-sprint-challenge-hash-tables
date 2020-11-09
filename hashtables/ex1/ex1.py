30df81067939d6d3bcfaa1a26c5f763d64e774c2
Craig Swanson <cswanson02@me.com>
2020-11-08 22:40:34 -0600

    Completed ex1 challenge

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,30 @@
+# this passes the tests but it doesn't seem quite right. Specifically, it seems
+# off for the cases that there are two+ items of the same weight.
+# when the array has two equal weights, there are many edge cases not covered.
+# say the limit is 8 and we are passed in [2, 6, 6, 6]; this does not handle that case.
+
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+
+    weights_dict = {}
+
+    for index, weight in enumerate(weights):
+        if weight in weights_dict:
+            weights_dict[weight].append(index)
+        else:
+            weights_dict[weight] = [index]
+
+    for weight, index in weights_dict.items():
+        if (limit - weight) in weights_dict:
+            if len(weights_dict[limit - weight]) > 1:
+                if weight == limit - weight:
+                    higher_index = weights_dict[limit - weight][1]
+                    lower_index = weights_dict[limit - weight][0]
+                    return (higher_index, lower_index)
+                else:
+                    return (weights_dict[limit - weight][1], weights_dict[weight][0])
+            else:
+                higher_index = weights_dict[limit - weight][0]
+                lower_index = weights_dict[weight][0]
+                return (higher_index, lower_index)
 
     return None
