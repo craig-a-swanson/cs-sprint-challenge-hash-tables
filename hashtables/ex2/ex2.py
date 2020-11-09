b24a0a9eb3b8f16f2a3e3157ddd5fd57cfc4b478
Craig Swanson <cswanson02@me.com>
2020-11-08 20:59:01 -0600

    Completed ex2 challenge

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,26 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    
+    # create array for the final route
+    route = []
+    # create a dictionary to store the source (key) and destination (value)
+    ticket_dict = {}
+
+    # iterate through the tickets array that was passed in
+    # add each ticket to the dictionary
+    for ticket in tickets:
+        source = ticket.source
+        dest = ticket.destination
+        ticket_dict[source] = dest
+    
+    # start at the current source of "NONE"
+    # for each ticket in the dictionary, append its destination to the route array
+    # then set the current_source equal to the ticket's destination and loop through
+    current_source = "NONE"
+    for _ in range(length):
+        current_ticket = ticket_dict[current_source]
+        route.append(current_ticket)
+        current_source = current_ticket
 
     return route
